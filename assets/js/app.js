8283cc4d2141c33dd79d311ec754e17b6d643b0d
Azimjon-exe <azimjonn2003@gmail.com>
2022-01-16 11:02:28 +0500

    a

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+$('#drop').click(function(){
+    $('.drop-t').toggle();
+    $('.drop').toggleClass('roted')
+})
+
+$('#drop1').click(function(){
+    $('.drop-t1').toggle();
+    $('.drop1').toggleClass('roted')
+})
+
+$('#drop2').click(function(){
+    $('.drop-t2').toggle();
+    $('.drop2').toggleClass('roted')
+})
+
+$('#drop3').click(function(){
+    $('.drop-t3').toggle();
+    $('.drop3 ').toggleClass('roted')
+})
+
+$('#drop4').click(function(){
+    $('.drop-t4').toggle();
+    $('.drop4 ').toggleClass('roted')
+})
+
+// $('#drop').click(function(){
+//     $(this).toggleClass('dropdow');
+//     $(this).toggleClass('roted')
+// })
+
+// $('#drop1').click(function(){
+//     $(this).toggleClass('dropdow');
+//     $(this).toggleClass('roted')
+// })
+
+// $('#drop2').click(function(){
+//     $(this).toggleClass('dropdow');
+//     $(this).toggleClass('roted')
+// })
+
+// $('#drop3').click(function(){
+//     $(this).toggleClass('dropdow');
+//     $(this).toggleClass('roted')
+// })
